042061980e010ee74fdc17e44e5bc85ef54e7855
Daniel <daniel850804@gmail.com>
2021-04-17 15:23:40 +0800

    need to pivot to library based

--- app/src/main/java/com/example/librarybookrecommendation/service/BookDispatcherService.kt
+++ app/src/main/java/com/example/librarybookrecommendation/service/BookDispatcherService.kt
@@ -7,46 +7,51 @@ import com.example.librarybookrecommendation.Util.kingStoneSeedLink
 import com.example.librarybookrecommendation.Util.kingStoneSeedLinkInLibrary
 import com.example.librarybookrecommendation.Util.scrappingProcessLog
 import com.example.librarybookrecommendation.database.AppDatabase
-import com.example.librarybookrecommendation.model.Book
-import com.example.librarybookrecommendation.model.EmptyBookToScrape
+import com.example.librarybookrecommendation.model.*
 import com.example.librarybookrecommendation.onlineBookStore.OnlineBookStore
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 object BookDispatcherService {
     fun launchDownloadingService(db: AppDatabase) {
         GlobalScope.launch {
             val bookDao = db.bookDao()
             val bookToScrapeDao = db.bookToScrapeDao()
+            val coroutineNumber = 5
 
             while (true){
                 /*general looper- unscraped links with default */
-                var listToScrape = bookToScrapeDao.getUnscraped(10)
+                var listToScrape = bookToScrapeDao.getUnscraped(coroutineNumber)
                 if (listToScrape.isEmpty())
                     listToScrape = listOf(EmptyBookToScrape)
 
-                /*loop through the list*/
-                for( bookToScrape in listToScrape){
-                    // get book and assign library to the book
-                    val (book, links) = OnlineBookStore.getOnlineStore(bookToScrape.link)
-                        .getBookAndFollowingLinks()
-                    val bookWithLibrary = book.copy(libraries = containingLibrary(isbn = book.ISBN))
+                val asyncList = listToScrape.map { async { processBook(it, bookDao, bookToScrapeDao) } }
 
-                    //store into persistence data storage with room
-                    bookDao.insert(bookWithLibrary)
-                    val bookToScrapeUpdated = bookToScrape.copy(scraped = true)
-                    bookToScrapeDao.update(bookToScrapeUpdated)
-                    bookToScrapeDao.insert(links)
-
-                    //log to see monitor the process
-                    Log.d(scrappingProcessLog, book.toString())
-                }
+                awaitAll(*asyncList.toTypedArray())
             }
+        }
+    }
 
+    private fun processBook(bookToScrape: BookToScrape, bookDao: BookDao, bookToScrapeDao: BookToScrapeDao){
+        try{
+            // get book and assign library to the book
+            val (book, links) = OnlineBookStore.getOnlineStore(bookToScrape.link)
+                .getBookAndFollowingLinks()
+            val bookWithLibrary = book.copy(libraries = containingLibrary(isbn = book.ISBN))
 
+            //store into persistence data storage with room
+            bookDao.insert(bookWithLibrary)
+            val bookToScrapeUpdated = bookToScrape.copy(scraped = true)
+            bookToScrapeDao.update(bookToScrapeUpdated)
+            bookToScrapeDao.insert(links)
 
-            val temp = bookDao.getAll()
-            temp
+            //log to see monitor the process
+            Log.d(scrappingProcessLog, book.toString())
+        }catch (e : Exception){
+            Log.d(scrappingProcessLog, "${bookToScrape.toString()} ${e.message}")
         }
     }
 }
