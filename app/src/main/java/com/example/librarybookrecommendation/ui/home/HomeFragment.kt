1f7ae76dd33cd845533f4d39b41b5e32b0b2da5a
Daniel <daniel850804@gmail.com>
2021-04-18 16:09:39 +0800

    solve refresh issue

--- app/src/main/java/com/example/librarybookrecommendation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/librarybookrecommendation/ui/home/HomeFragment.kt
@@ -24,6 +24,7 @@ import com.example.librarybookrecommendation.ui.adapter.BookGridAdapter
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    var bookList = listOf<Book>()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -56,10 +57,13 @@ class HomeFragment : Fragment() {
         })
 
         homeViewModel.bookList.observe(viewLifecycleOwner, Observer {
+            bookList = it
             bookGridAdapter.setData(it)
         })
 
-        homeViewModel.refreshBooks()
+        if(bookList.isEmpty()){
+            homeViewModel.refreshBooks()
+        }
 
         return root
     }
